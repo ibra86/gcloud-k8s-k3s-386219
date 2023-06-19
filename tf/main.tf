bf58a8e992642d145e8277d9a8344c11ef6f5c25
Anton Ibragimov <ibrahimov_ag@cloudshell>
2023-06-19 13:57:17 +0000

    replace gke by kind resource

--- tf/main.tf
+++ tf/main.tf
@@ -7,18 +7,24 @@ module "github_repository" {
   public_key_openssh_title = "flux0"
 }
 
-module "gke_cluster" {
-  source         = "github.com/den-vasyliev/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 1
+
+module "kind_cluster" {
+  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
 }
 
 module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  config_path       = module.gke_cluster.kubeconfig
+  private_key       = module.tls_private_key.private_key_pem
+
+  config_host       = module.kind_cluster.endpoint
+  config_client_key = module.kind_cluster.client_key
+  config_ca         = module.kind_cluster.ca
+  config_crt        = module.kind_cluster.crt
+
+  github_token = var.GITHUB_TOKEN
 }
+
 
 module "tls_private_key" {
   source    = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
